1e177c6ce458e77f680cb5d709daaac1b81d3715
7oh4nn <62851611+7oh4nn@users.noreply.github.com>
2020-04-08 15:13:43 +0200

    SASS + components
    added first SASS and created first UI components.

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>To Do</h1>
+    <span>Organize your life</span>
+  </header>
+);
+export default Header;
---
+++ src/components/ToDosItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ToDosItem = () => (
+  <section>
+    <ul>
+      <li class="todoitem">
+        <h2>Item Name</h2>
+        <p>Description</p>
+        <span>Date</span>
+      </li>
+      <li class="todoitem">
+        <h2>Item Name</h2>
+        <p>Description</p>
+        <span>Date</span>
+      </li>
+    </ul>
+  </section>
+);
+export default ToDosItem;
